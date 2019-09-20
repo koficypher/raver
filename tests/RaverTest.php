ceb6bfd3e0ca6b833fedc298bc93ce837c2416ed
Kofi Cypher <skcypher6@gmail.com>
2019-09-20 16:10:01 +0000

    Apply fixes from StyleCI (#6)

--- tests/RaverTest.php
+++ tests/RaverTest.php
@@ -1,12 +1,21 @@
-<?php 
+<?php
+
+/*
+ * Raver - Unofficial Rave PHP SDK
+ *
+ * Copyright - 2019
+ *
+ * MIT
+ *
+ * Kofi Cypher <skcypher6@gmail.com>
+ */
+
 namespace Tests;
 
+use Raver\Config\Config;
 use PHPUnit\Framework\TestCase;
 
-use Raver\Raver;
-use Raver\Config\Config;
-
-class RaverTest extends TestCase 
+class RaverTest extends TestCase
 {
     public function testTruthy()
     {
@@ -15,31 +24,25 @@ class RaverTest extends TestCase
 
     public function testPrepareData()
     {
-      $config = new Config();
-      $var_keys = $config->getEnvVars();
+        $config = new Config();
+        $var_keys = $config->getEnvVars();
 
-      $card  =  array(
-                "PBFPubKey" => $var_keys['public_key'] ,
-                "cardno" => "4242424242424242",
-                "currency" => "GHS",
-                "country" => "GH",
-                "cvv" => "812",
-                "amount" => "100",
-                "expiryyear" => "21",
-                "expirymonth" => "01",
-                "email" => "skafui@gmail.com",
-                "phonenumber" => "0201478963",
-                "firstname" => "Selorm",
-                "lastname" => "Kafui",
-                "IP"=> "355426087298442",
-                "txRef" => "MC-". time()
-            );
-      $this->assertIsArray($card);
-
+        $card = [
+                'PBFPubKey' => $var_keys['public_key'],
+                'cardno' => '4242424242424242',
+                'currency' => 'GHS',
+                'country' => 'GH',
+                'cvv' => '812',
+                'amount' => '100',
+                'expiryyear' => '21',
+                'expirymonth' => '01',
+                'email' => 'skafui@gmail.com',
+                'phonenumber' => '0201478963',
+                'firstname' => 'Selorm',
+                'lastname' => 'Kafui',
+                'IP'=> '355426087298442',
+                'txRef' => 'MC-'.time(),
+            ];
+        $this->assertIsArray($card);
     }
-
-
 }
-
-
-?>
