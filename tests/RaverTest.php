58aa7fdde5dcfec1876d1e97e3debabaf7d5cf67
Sitsofe K. Fefeti <skcypher6@gmail.com>
2019-11-08 16:35:05 +0000

    removing instance tests

--- tests/RaverTest.php
+++ tests/RaverTest.php
@@ -22,35 +22,4 @@ class RaverTest extends TestCase
     {
         $this->assertTrue(true);
     }
-
-    public function testApi()
-    {
-        $raver = new Raver();
-        $text = $raver->testApi();
-        $this->assertSame('Yes, am working now', $text);
-    }
-
-    public function testPrepareData()
-    {
-        $config = new Config();
-        $var_keys = $config->getEnvVars();
-
-        $card = [
-                'PBFPubKey' => $var_keys['public_key'],
-                'cardno' => '4242424242424242',
-                'currency' => 'GHS',
-                'country' => 'GH',
-                'cvv' => '812',
-                'amount' => '100',
-                'expiryyear' => '21',
-                'expirymonth' => '01',
-                'email' => 'skafui@gmail.com',
-                'phonenumber' => '0201478963',
-                'firstname' => 'Selorm',
-                'lastname' => 'Kafui',
-                'IP'=> '355426087298442',
-                'txRef' => 'MC-'.time(),
-            ];
-        $this->assertIsArray($card);
-    }
 }
