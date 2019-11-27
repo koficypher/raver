fc810cdce54b07a14f19e01fa108753c454b6f65
Sitsofe K. Fefeti <skcypher6@gmail.com>
2019-11-27 15:29:31 +0000

    updated workflow with secrets

--- tests/RaverTest.php
+++ tests/RaverTest.php
@@ -14,10 +14,19 @@ namespace Raver\Tests;
 
 use PHPUnit\Framework\TestCase;
 
+use Raver\Executor;
+
 class RaverTest extends TestCase
 {
     public function testTruthy()
     {
         $this->assertTrue(true);
+    }
+
+    public function testExecutorInstance()
+    {
+      $executor  = new Executor();
+
+      $this->assertInstanceOf(Executor::class, $executor);
     }
 }
